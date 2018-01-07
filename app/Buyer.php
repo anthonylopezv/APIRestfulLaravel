f09adcbf9d55e108c533d80cc42d9896e7f4c5f7
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2018-01-07 12:21:26 -0500

    Implementando los Modelos de la API RESTful y sus Relaciones con Eloquent

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,7 +2,11 @@
 
 namespace App;
 
+use App\Transaction;
+// Comprador
 class Buyer extends User
 {
-    //
+    public function transactions(){
+        return $this-> hasMany(Transaction::class);
+    }
 }
