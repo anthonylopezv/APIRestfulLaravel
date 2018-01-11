07a645600bd17f33f059fd7e4810f4606c038bc2
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2018-01-11 15:55:52 -0500

    guardando cambios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,119 +1,65 @@
 <?php
 
-// use App\User;
-// use App\Seller;
-// use App\Product;
-// use App\Category;
-// use App\Transaction;
-
-
-// use Faker\Generator as Faker;
-
-// /*
-// |--------------------------------------------------------------------------
-// | Model Factories
-// |--------------------------------------------------------------------------
-// |
-// | This directory should contain each of the model factory definitions for
-// | your application. Factories provide a convenient way to generate new
-// | model instances for testing / seeding your application's database.
-// |
-// */
-
-// $factory->define(User::class, function (Faker $faker) {
-//     return [
-//         'name' => $faker->name,
-//         'email' => $faker->unique()->safeEmail,
-//         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-//         'remember_token' => str_random(10),
-//         'verified' => $verificado = $faker->randomElement([User::USUARIO_VERIFICADO, User::USUARIO_NO_VERIFICADO]),
-//         'verification_token' => $verificado == User::USUARIO_VERIFICADO ? null : User::generarVerificationToken(),
-//         'admin' => $faker->randomElement([User::USUARIO_ADMINISTRADOR, User::USUARIO_REGULAR]),
-//     ];
-// });
-
-// $factory->define(Category::class, function (Faker $faker) {
-//     return [
-//         'name' => $faker->word,
-//         'description' => $faker->paragraph(1),
-//     ];
-// });
-
-// $factory->define(Product::class, function (Faker $faker) {
-//     return [
-//         'name' => $faker->word,
-//         'description' => $faker->paragraph(1),
-//         'quantity' => $faker->numberBetween(1,10),
-//         'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),//Elemento aleatorio
-//         'image' => $faker->randomElement(['product1.jpg', 'product2.jpg', 'product3.jpg']),
-//         //'seller_id' => User::inRandomOrder()->first()->id,
-//         'seller_id' => User::all()->random()->id,
-//     ];
-// });
-
-// $factory->define(Transaction::class, function (Faker $faker) {
-
-//     $vendedor = Seller::has('products')->get()->random();
-//     $comprador = User::all()->except($vendedor->id)->random();
-
-//     return [
-//         'quantity' => $faker->numberBetween(1,3),
-//         'buyer_id' => $comprador->id,
-//         'product_id' => $vendedor->products->random()->id,
-//     ];
-// });
-
 use App\User;
 use App\Seller;
 use App\Product;
 use App\Category;
 use App\Transaction;
+
+
+use Faker\Generator as Faker;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
 |--------------------------------------------------------------------------
 |
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
 |
 */
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(User::class, function (Faker\Generator $faker) {
-    static $password;
+
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
         'verified' => $verificado = $faker->randomElement([User::USUARIO_VERIFICADO, User::USUARIO_NO_VERIFICADO]),
         'verification_token' => $verificado == User::USUARIO_VERIFICADO ? null : User::generarVerificationToken(),
         'admin' => $faker->randomElement([User::USUARIO_ADMINISTRADOR, User::USUARIO_REGULAR]),
     ];
 });
-$factory->define(Category::class, function (Faker\Generator $faker) {
+
+$factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
     ];
 });
-$factory->define(Product::class, function (Faker\Generator $faker) {
+
+$factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
-        'quantity' => $faker->numberBetween(1, 10),
-        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
-        'image' => $faker->randomElement(['1.jpg', '2.jpg', '3.jpg']),
-        // 'seller_id' => User::inRandomOrder()->first()->id,
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),//Elemento aleatorio
+        'image' => $faker->randomElement(['product1.jpg', 'product2.jpg', 'product3.jpg']),
+        //'seller_id' => User::inRandomOrder()->first()->id,
         'seller_id' => User::all()->random()->id,
     ];
 });
-$factory->define(Transaction::class, function (Faker\Generator $faker) {
-	$vendedor = Seller::has('products')->get()->random();
-	$comprador = User::all()->except($vendedor->id)->random();
+
+$factory->define(Transaction::class, function (Faker $faker) {
+
+    $vendedor = Seller::has('products')->get()->random();
+    $comprador = User::all()->except($vendedor->id)->random();
+
     return [
-        'quantity' => $faker->numberBetween(1, 3),
+        'quantity' => $faker->numberBetween(1,3),
         'buyer_id' => $comprador->id,
         'product_id' => $vendedor->products->random()->id,
     ];
 });
+
